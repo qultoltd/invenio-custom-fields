c3391b8650d0ce88e0956fa3e4b74f03bc4bf90b
Horváth-Gyurcsik Dóra <dgyurcsik@monguz.hu>
2025-03-20 11:55:55 +0100

    SZTERRF-26 revert accordion field mr fix

--- invenio_custom_fields/assets/semantic-ui/js/invenio_custom_fields/components/react_invenio_forms/AccordionField.js
+++ invenio_custom_fields/assets/semantic-ui/js/invenio_custom_fields/components/react_invenio_forms/AccordionField.js
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import { Field, FastField } from "formik";
 import { Accordion, Button, Container, Icon } from "semantic-ui-react";
 import { i18next } from "@translations/invenio_app_rdm/i18next";
 import _omit from "lodash/omit";
 import _get from "lodash/get";
 
-const AccordionField = ({
-  includesPaths = [],
-  optimized,
-  label = "",
-  children,
-  active = true,
-  deletable,
-  onDeleteSection,
-  ...uiProps
-}) => {
-  const [activeIndex, setActiveIndex] = useState(active ? 0 : -1);
-
-  const hasError = (errors, initialValues = undefined, values = undefined) => {
+class AccordionField extends Component {
+  hasError(errors, initialValues = undefined, values = undefined) {
+    const { includesPaths } = this.props;
     for (const errorPath in errors) {
       for (const subPath in errors[errorPath]) {
         const path = `${errorPath}.${subPath}`;
@@ -29,25 +19,21 @@ const AccordionField = ({
       }
     }
     return false;
-  };
+  }
 
-  const handleTitleClick = (e, { index }) => {
-    setActiveIndex(activeIndex === index ? -1 : index);
-  };
+  renderAccordion = (props) => {
+    const {
+      form: { errors, status, initialErrors, initialValues, values },
+    } = props;
 
-  const handleKeyDownTitleClick = (e, index) => {
-    if (e.key === "Enter" || e.key === " ") {
-      handleTitleClick(e, { index });
-    }
-  };
-
-  const renderAccordion = ({
-    form: { errors, status, initialErrors, initialValues, values },
-  }) => {
-    const accordionHasError = status
-      ? hasError(status)
-      : hasError(errors) || hasError(initialErrors, initialValues, values);
-
+    // eslint-disable-next-line no-unused-vars
+    const { label, children, active, deletable, onDeleteSection, ...ui } =
+      this.props;
+    const uiProps = _omit({ ...ui }, ["optimized", "includesPaths"]);
+    const hasError = status
+      ? this.hasError(status)
+      : this.hasError(errors) ||
+        this.hasError(initialErrors, initialValues, values);
     const panels = [
       {
         key: `panel-${label}`,
@@ -60,7 +46,12 @@ const AccordionField = ({
       },
     ];
 
-    const errorClass = accordionHasError ? "error secondary" : "";
+    const errorClass = hasError ? "error secondary" : "";
+    const [activeIndex, setActiveIndex] = useState(active ? 0 : -1);
+
+    const handleTitleClick = (e, { index }) => {
+      setActiveIndex(activeIndex === index ? -1 : index);
+    };
 
     return (
       <Accordion
@@ -74,7 +65,11 @@ const AccordionField = ({
               active={activeIndex === index}
               index={index}
               onClick={handleTitleClick}
-              onKeyDown={(e) => handleKeyDownTitleClick(e, index)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                  handleTitleClick(e, { index });
+                }
+              }}
               tabIndex={0}
             >
               {panel.title.content}
@@ -89,7 +84,6 @@ const AccordionField = ({
               )}
               <Icon name="angle right" />
             </Accordion.Title>
-
             <Accordion.Content active={activeIndex === index}>
               {panel.content.content}
             </Accordion.Content>
@@ -99,8 +93,21 @@ const AccordionField = ({
     );
   };
 
-  const FormikField = optimized ? FastField : Field;
-  return <FormikField name="" component={renderAccordion} />;
+  render() {
+    const { optimized } = this.props;
+
+    const FormikField = optimized ? FastField : Field;
+    return <FormikField name="" component={this.renderAccordion} />;
+  }
+}
+
+AccordionField.defaultProps = {
+  active: true,
+  includesPaths: [],
+  label: "",
+  optimized: false,
+  children: null,
+  ui: null,
 };
 
 export default AccordionField;
